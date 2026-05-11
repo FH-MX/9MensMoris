eb7bf0014cc7913dcf107b5937dda7ce4bb849ce
FH-MX <zvd8npb54d@privaterelay.appleid.com>
2026-05-11 13:16:52 +0900

    built web by peanut
    Branch: main
    Commit: 089d3541e4f1490a3e927f6d1b6b5419224a3122
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "399555079" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1787068366" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
